d84b620de7c75032a1238406f871378d88519473
Bogdan Markin <bogdan.flam@mail.ru>
2022-11-08 21:24:33 +0300

    add brain_games/scripts/brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.logic_game_even import user_game
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    user_game()
+
+
+if __name__ == '__main__':
+    main()

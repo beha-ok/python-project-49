9a622b692adb4aee6c35af69995dbdd4fadc0abc
Bogdan Markin <bogdan.flam@mail.ru>
2022-11-11 00:05:08 +0300

    add brain_games/games/

---
+++ brain_games/games/logic_calc.py
@@ -0,0 +1,14 @@
+from random import choice, randint
+
+game_offer = 'What is the result of the expression?'
+
+def raund():
+    game_offer
+    operators = ['*', '+', '-']
+    random_operator = choice(operators)
+    num_1 = randint(0, 10)
+    num_2 = randint(0, 10)
+    questions = (f'{num_1} {random_operator} {num_2}')
+    result_questions = eval(f'{num_1} {random_operator} {num_2}')
+    return questions, str(result_questions)
+

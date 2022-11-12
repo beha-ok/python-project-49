54d853e31a9d58f47bf0c8c378cb52bde6915658
Bogdan Markin <bogdan.flam@mail.ru>
2022-11-12 22:46:07 +0300

    add file brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+from brain_games import engine
+from brain_games.games import game_progression
+
+
+def main():
+    engine.engine_games(game_progression)
+
+
+if __name__ == '__main__':
+    main()
+    

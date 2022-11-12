97e0ea99bcaf40cd43f516e51d5a8c3f4feee2b9
Bogdan Markin <bogdan.flam@mail.ru>
2022-11-12 12:43:20 +0300

    rename "logic" in "game"

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from brain_games import engine
-from brain_games.games import logic_calc
+from brain_games.games import game_calc
 
 
 def main():
-    engine.engine_games(logic_calc)
+    engine.engine_games(game_calc)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from brain_games import engine
-from brain_games.games import logic_even
+from brain_games.games import game_even
 
 
 def main():
-    engine.engine_games(logic_even)
+    engine.engine_games(game_even)
 
 
 if __name__ == '__main__':
